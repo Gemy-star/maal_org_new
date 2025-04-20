670bb4733793c107570f315cb1734cf2cb7f9cc0
Mohamed Ismail <mohamed.g.ismail@orange.com>
2025-04-20 17:20:28 +0200

    add orgs

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Projects)
+admin.site.register(models.Blog)
+admin.site.register(models.Contact)
+admin.site.register(models.Announcement)
+admin.site.register(models.Category)
